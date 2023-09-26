3a29e8ec93e531f19b7a19ade8da648cec2db11b
Alan Shieh <piratejing41@gmail.com>
2023-09-26 16:45:21 -0700

    added :

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <footer className="footer">
-      <div className="footer__author">Developed by Alan Shieh</div>
+      <div className="footer__author">Developed by: Alan Shieh</div>
       <div className="footer__year">2023</div>
     </footer>
   );
